cebfa3e309e380d58ed85de6906764aa010b79e9
Pagunasa <Pagunasa@DESKTOP-DGB2C4I>
2017-04-23 11:30:03 +0200

    First commit, init of the project

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Pagunasa
+ *
+ * Created on 23 de abril de 2017, 11:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
