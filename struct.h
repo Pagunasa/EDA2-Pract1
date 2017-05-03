9defdf3dae481a80be62236828f44c4a2a973d64
u137557 <u137557@54006p07.ds.upf.edu>
2017-05-03 11:36:14 +0200

    structs data

---
+++ struct.h
@@ -0,0 +1,24 @@
+#ifndef STRUCTS_H
+#define	STRUCTS_H
+
+typedef struct{
+    char name[MAX_LENGTH10];
+    char surname[MAX_LENGTH10];
+    char sex;
+    int day;
+    int month;
+    int year;
+}sdata;
+
+typedef struct node{
+    int DNI;
+    sdata Info;
+    node *next;
+}snode;
+
+typedef struct {
+    snode *first;
+    int size;
+}slist;
+
+#endif	/* STRUCTS_H */
